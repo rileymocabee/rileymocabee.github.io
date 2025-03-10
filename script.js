84f0d9d1ef43099038aee6bb64489692fe02caea
Riley Mocabee <rileymocabee@Rileys-MacBook-Pro-2.local>
2025-03-10 16:03:46 -0700

    Update

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
   const appearOnScroll = new IntersectionObserver((entries, observer) => {
     entries.forEach(entry => {
-      if(!entry.isIntersecting) return;
+      if (!entry.isIntersecting) return;
       entry.target.classList.add('fade-in-active');
       observer.unobserve(entry.target);
     });
